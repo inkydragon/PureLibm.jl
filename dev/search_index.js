e5cea23716f2856168a684746da14eb0a80a5a5c
Documenter.jl <documenter@juliadocs.github.io>
2024-02-03 22:15:47 +0000

    build based on 42bcff1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PureLibm","category":"page"},{"location":"#PureLibm","page":"Home","title":"PureLibm","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PureLibm.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PureLibm]","category":"page"}]
+}
