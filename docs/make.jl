c11680ad353863afb7871a9c48c69f8d3dbc6604
Chengyu HAN <git@wo-class.cn>
2024-05-12 23:46:08 -0500

    doc: add BSD Libm

--- docs/make.jl
+++ docs/make.jl
@@ -16,11 +16,15 @@ makedocs(;
         "Home" => "index.md",
         "Reference" => Any[
             "Reference" => "ref/references.md",
+            # cr
             "LLVM Libc [Apache]" => "ref/llvm-libc.md",
             "Core Math [MIT]" => "ref/core-math.md",
             "RLibm [MIT]" => "ref/rlibm.md",
+            # libm
+            "BSD Libm [BSD]" => "ref/bsd.md",
             "Zig [MIT]" => "ref/zig.md",
             "AOCL-LibM [BSD-3]" => "ref/aocl.md",
+            # C Std
             "Std C11" => "ref/std-c11.md",
             "Std C23" => "ref/std-c23.md",
         ],
---
+++ docs/src/ref/bsd.md
@@ -0,0 +1,3 @@
+# BSD Libm [BSD]
+> - [lib/msun - FreeBSD](https://github.com/freebsd/freebsd-src/tree/main/lib/msun)
+> - [lib/libm - OpenBSD](https://github.com/openbsd/src/tree/master/lib/libm)
