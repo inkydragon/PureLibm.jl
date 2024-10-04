c1eb31912836c873c19da89e78345471164c3be7
Chengyu HAN <git@wo-class.cn>
2024-10-04 20:13:35 +0800

    test: merge sqrt test

--- test/sqrt.jl
+++ test/sqrt.jl
@@ -1,34 +1,17 @@
 
-@testset "sqrt(::Float32)" begin
-    T = Float32
-
-    # IEC 60559
-    @test isnan(PureLibm.sqrt(T(-1.0)))
-    @test isnan(PureLibm.sqrt(T(-Inf)))
-    # @test_throws DomainError PureLibm.sqrt(T(-1.0))
-    @test PureLibm.sqrt(T(Inf)) == T(Inf)
-    @test PureLibm.sqrt(T(0.0)) == T(0.0)
-    @test PureLibm.sqrt(T(-0.0)) == T(0.0)
-    @test isnan(PureLibm.sqrt(T(NaN)))
-    
-    # sanity check
-    @test PureLibm.sqrt(T(100.0)) == T(10.0)
-    @test PureLibm.sqrt(T(4.0)) == T(2.0)
-end
-
-@testset "sqrt(::Float64)" begin
-    T = Float64
-
-    # IEC 60559
-    @test isnan(PureLibm.sqrt(T(-1.0)))
-    @test isnan(PureLibm.sqrt(T(-Inf)))
-    # @test_throws DomainError PureLibm.sqrt(T(-1.0))
-    @test PureLibm.sqrt(T(Inf)) == T(Inf)
-    @test PureLibm.sqrt(T(0.0)) == T(0.0)
-    @test PureLibm.sqrt(T(-0.0)) == T(0.0)
-    @test isnan(PureLibm.sqrt(T(NaN)))
-    
-    # sanity check
-    @test PureLibm.sqrt(T(100.0)) == T(10.0)
-    @test PureLibm.sqrt(T(4.0)) == T(2.0)
+for T in [Float32, Float64]
+    @testset "sqrt(::$T)" begin
+        # IEC 60559
+        @test isnan(PureLibm.sqrt(T(-1.0)))
+        @test isnan(PureLibm.sqrt(T(-Inf)))
+        # @test_throws DomainError PureLibm.sqrt(T(-1.0))
+        @test PureLibm.sqrt(T(Inf)) == T(Inf)
+        @test PureLibm.sqrt(T(0.0)) == T(0.0)
+        @test PureLibm.sqrt(T(-0.0)) == T(0.0)
+        @test isnan(PureLibm.sqrt(T(NaN)))
+        
+        # sanity check
+        @test PureLibm.sqrt(T(100.0)) == T(10.0)
+        @test PureLibm.sqrt(T(4.0)) == T(2.0)
+    end
 end
