8609f1c2942e785aab0cf4cc62007b9918927ff4
Chengyu HAN <git@wo-class.cn>
2024-10-07 18:22:56 +0800

    utils.hint: add branch prediction annotations

--- src/utils/hint.jl
+++ src/utils/hint.jl
@@ -1,7 +1,27 @@
 
+## branch prediction annotations
+
 """
-#define likely(x)      __builtin_expect(!!(x), 1)
-#define unlikely(x)    __builtin_expect(!!(x), 0)
+Hints that the condition `cond` is likely to be `true`.
+
+```c
+// Macro define in C
+#define likely(x)   __builtin_expect(!!(x), 1)
+```
+"""
+macro likely(cond)
+    # TODO: Maybe use `@llvm.expect.i1`
+    # declare i1 @llvm.expect.i1(i1 <val>, i1 <expected_val>)
+    return :( $(esc(cond)) )
+end
+
+"""
+Hints that the condition `cond` is likely to be `false`.
+
+```c
+// Macro define in C
+#define unlikely(x) __builtin_expect(!!(x), 0)
+```
 """
 macro unlikely(cond)
     return :( $(esc(cond)) )
