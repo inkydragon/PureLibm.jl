bddbe84b75499fc45815961898219e895926b391
Chengyu HAN <git@wo-class.cn>
2024-10-07 16:55:11 +0800

    rsqrtf: add test

---
+++ test/rsqrt.jl
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+for T in [Float32, ]
+    @testset "rsqrt(::$T)" begin
+        # IEC 60559
+        # rSqrt(+∞) is +0 with no exception. 
+        @test PureLibm.rsqrt(T(+Inf)) ≈ T(+0.0)
+        # rSqrt(±0) is ±∞ and signals the divideByZero exception.
+        @test PureLibm.rsqrt(T(+0.0)) ≈ T(+Inf)
+        @test PureLibm.rsqrt(T(-0.0)) ≈ T(-Inf)
+        @test isnan(PureLibm.rsqrt(T(NaN)))
+
+        # sanity check
+        @test isnan(PureLibm.rsqrt(T(-1)))
+        @test isnan(PureLibm.rsqrt(T(-Inf)))
+        @test PureLibm.rsqrt(T(1)) ≈ T(1)
+        @test PureLibm.rsqrt(T(4)) ≈ T(0.5)
+    end
+end
