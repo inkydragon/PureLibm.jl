9e93802e219794e7eb4fbd17024dc27da6029e02
Chengyu HAN <git@wo-class.cn>
2024-10-07 21:46:53 +0800

    tgamma: add ieee test

--- test/tgamma.jl
+++ test/tgamma.jl
@@ -3,6 +3,15 @@
 @testset "tgamma" begin
     @testset "$T" for T in [Float32, ]
         # IEC 60559
+        @test PureLibm.tgamma(T(Inf)) == T(Inf)
+        # fp-invalid
+        @test isnan(PureLibm.tgamma(T(-Inf)))
+        @test isnan(PureLibm.tgamma(T(-1.0)))
+        # fp-divide-by-zero
+        @test PureLibm.tgamma(T(+0.0)) == T(+Inf)
+        @test PureLibm.tgamma(T(-0.0)) == T(-Inf)
+        
+        # sanity check
         # TODO
     end
 end
