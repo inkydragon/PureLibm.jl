3d483ae6b4417c61109a44a214789abbdf22ff1e
Chengyu HAN <cyhan.dev@outlook.com>
2024-10-16 21:14:23 +0800

    test.acosf: add tests

--- test/acos.jl
+++ test/acos.jl
@@ -16,3 +16,50 @@ for T in [Float32, ]
         @test PureLibm.acos(T(0.5)) * 3 ≈ pi
     end
 end
+
+
+if "acosf.fast" in CheckExhaustive
+@testset "acosf-exhaustive.fast" begin
+    xlo = reinterpret(UInt32, Float32(0.0))
+    xhi = reinterpret(UInt32, Float32(1.0))
+    for xu in xlo:xhi, sign in [1, -1]
+        x = reinterpret(Float32, xu)
+        x = copysign(x, sign)
+        y = PureLibm.acos(x)
+        z = acos(x)
+
+        if isnan(z) && isnan(y)
+            continue
+        elseif isinf(z) && isinf(y)
+            continue
+        elseif z ≈ y
+            continue
+        else
+            @printf("[xu = 0x%x (%e)]:  y=%e; z=%e\n", xu, x, y, z)
+        end
+    end
+    println("test $(length(xlo:xhi)*2) cases")
+end
+end # CheckExhaustive
+
+if "acosf" in CheckExhaustive
+@testset "acosf-exhaustive" begin
+    xlo = reinterpret(UInt32, Float32(0.0))
+    xhi = reinterpret(UInt32, Float32(1.0))
+    for xu in xlo:xhi, sign in [1, -1]
+        x = reinterpret(Float32, xu)
+        x = copysign(x, sign)
+        y = PureLibm.acos(x)
+        z = Float32(acos(BigFloat(x)))
+
+        if y === z
+            continue
+        else
+            @printf("[xu = 0x%x (%e)]:  y=%e; z=%e\n", xu, x, y, z)
+        end
+    end
+    println("test $(length(xlo:xhi)*2) cases")
+end
+
+end # CheckExhaustive
+# ENV["PURELIBM_CHECK_EXHAUSTIVE"] = "acosf.fast,acosf"
