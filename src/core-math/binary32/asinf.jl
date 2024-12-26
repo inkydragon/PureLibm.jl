70d8e9f50fcbc8862bc8bd34989d74ade6bef9b7
Chengyu Han <cyhan.dev@outlook.com>
2024-12-26 17:37:33 +0800

    asin: bugfix ploy arg CR_ASINF_B and fma arg type (#20)
    * test.asin: test branch cov
    
    * asin: bugfix fma arg type
    
    * asin: bugfix ploy arg CR_ASINF_B
    
    * test.asin: x in [0.0, 0.5]
    
    * asin: make branch likely

--- src/core-math/binary32/asinf.jl
+++ src/core-math/binary32/asinf.jl
@@ -43,13 +43,15 @@ function cr_asinf(x::Float32)
     t = reinterpret(UInt32, x)
     ax = t << UInt32(1)
 
-    if ax > (0x0000_007f << 24)
+    if @unlikely(ax > (0x0000_007f << 24))
+        # abs(x) > 1.0
         return _asinf_as_special(x)
     end
 
-    if ax < 0x7ec29000
-        if ax < UInt32(115 << 24)
-            return fma(x, 0x1p-25, x)
+    if @likely(ax < 0x7ec29000)
+        # abs(x) < 0.8800049f0
+        if @unlikely(ax < UInt32(115 << 24))
+            return fma(x, Float32(0x1p-25), x)
         end
 
         z = xs
@@ -59,10 +61,12 @@ function cr_asinf(x::Float32)
         z16 = z8 * z8
         b = CR_ASINF_B
         r = z * (
-            (((b[1] + z2 * b[2]) + z4 * (b[3] + z2 * b[4])) + 
-                z8 * ((b[5] + z2 * b[6]) + z4 * (b[7] + z2 * b[8]))) +
-            z16 * (((b[9] + z2 * b[10]) + z4 * (b[11] + z2 * b[12])) + 
-                    z8 * ((b[13] + z2 * b[14]) + z4 * b[15])))
+            (((b[1] + z2*b[2]) + z4*(b[3] + z2*b[4]))
+                + z8*((b[5] + z2*b[6]) + z4*(b[7] + z2*b[8])))
+            + z16*(
+                ((b[9] + z2*b[10]) + z4*(b[11] + z2*b[12]))
+                + z8*((b[13] + z2*b[14]) + z4*(b[15] + z2*b[16])))
+        )
         ub = Float32(r)
         lb = Float32(r - z * 0x1.efa8ebp-31)
         if ub == lb
@@ -71,15 +75,16 @@ function cr_asinf(x::Float32)
     end
 
     if ax < (0x0000_007e << 24)
+        # abs(x) < 0.5
         z = xs
         z2 = z * z
         c0 = poly12(z2, CR_ASINF_C1)
         r = z + (z * z2) * c0
     else
-        if ax == 0x7e55688a
+        if @unlikely(ax == 0x7e55688a)  # 0.6668132f0
             return copysign(Float32(0x1.75b8a2p-1), x) + copysign(Float32(0x1p-26), x)
         end
-        if ax == 0x7e107434
+        if @unlikely(ax == 0x7e107434)  # 0.53213656f0
             return copysign(Float32(0x1.1f4b64p-1), x) + copysign(Float32(0x1p-26), x)
         end
 
--- test/asin.jl
+++ test/asin.jl
@@ -16,4 +16,24 @@ for T in [Float32, ]
         @test PureLibm.cr_asin(T(1.0)) * 2 ≈ pi
         @test PureLibm.cr_asin(T(-0.5)) * 6 ≈ -pi
     end
+
+    @testset "cr_asin(random)" begin
+        test_x = T[
+            eps(T(0.0)),
+            (0.0:0.05:1.0)...,
+            # rand(0.0:eps(T):1.0, 10)...,
+            # Branch cov
+            0.6668132f0, 0.53213656f0,
+            # ub != lb && ax < (0x0000_007e << 24)
+            #   x in [0.0, 0.5]
+            reinterpret.(Float32, rand(UInt32(0):0x3f000000, 10))...,
+        ]
+        test_x = [test_x..., -test_x...]
+        @testset "cr_asin($x)" for x in test_x
+            # Test against system libm
+            @test PureLibm.cr_asin(x) ≈ asin(x)
+            # Test against MPFR
+            @test PureLibm.cr_asin(x) === T(asin(BigFloat(x)))
+        end
+    end
 end
