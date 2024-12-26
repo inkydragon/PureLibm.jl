26adfa68f75f348d11fdc3f84c248a9cdc1c6ea1
Chengyu Han <cyhan.dev@outlook.com>
2024-12-26 16:17:08 +0800

    rsqrt: fix TypeError and xor use (#18)
    * test.rsqrt: add Branch cov tests
    
    * test.rsqrt: add rand tests
    
    * rsqrt: bugfix TypeError
    
    * test.rsqrt: add normal tests
    
    * rsqrt: bugfix xor
    
    * rsqrt: bugfix Int64 mod
    
    * rsqrt: remove debug show
    
    * rsqrt: simplify fast return
    
    * rsqrt: bugfix xor
    
    * test.rsqrt: remove neg tests

--- test/rsqrt.jl
+++ test/rsqrt.jl
@@ -17,6 +17,27 @@ for T in [Float32, Float64]
         @test PureLibm.cr_rsqrt(T(1)) ≈ T(1)
         @test PureLibm.cr_rsqrt(T(4)) ≈ T(0.5)
     end
+
+    @testset "cr_rsqrt(random)" begin
+        test_x = T[
+            (0.0:0.1:5.0)...,
+            rand(0.0:eps():1.0, 10)...,
+            # Branch cov
+            # ixu < (UInt64(1) << 52) && ixu != 0
+            eps(T(0.0)),
+        ]
+        @testset "cr_rsqrt($x)" for x in test_x
+            if x < 0
+                @test PureLibm.cr_rsqrt(x) === -T(NaN)
+                continue
+            end
+
+            # Test against system libm
+            @test PureLibm.cr_rsqrt(x) ≈ 1/sqrt(x)
+            # Test against MPFR
+            @test PureLibm.cr_rsqrt(x) === T(1/sqrt(BigFloat(x)))
+        end
+    end
 end
 
 @testset "cr_rsqrt.special-case" begin
