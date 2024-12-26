05bb24cf2f4689ffb21f1ce791a1ab454357d55c
Chengyu Han <cyhan.dev@outlook.com>
2024-12-26 16:36:03 +0800

    Remove openLibm and utils/error.jl (#19)
    * utils: remove error.jl
    
    * OpenLibm: remove all functions

--- src/PureLibm.jl
+++ src/PureLibm.jl
@@ -2,11 +2,9 @@ module PureLibm
 
 include("utils/const.jl")
 include("utils/llvm_intrinsics.jl")
-include("utils/error.jl")
 include("utils/hint.jl")
 
 # impl
-include("openlibm/OpenLibm.jl")
 include("core-math/CoreMath.jl")
 
 include("doc.jl")
--- test/cbrt.jl
+++ test/cbrt.jl
@@ -2,20 +2,20 @@
 
 # if the argument is ±0 or ±∞, it is returned, unchanged
 # if the argument is NaN, NaN is returned.
-for T in [Float32, Float64]
-    @testset "musl_cbrt(::$T)" begin
-        # IEC 60559
-        @test PureLibm.musl_cbrt(T(0.0)) == T(0.0)
-        @test PureLibm.musl_cbrt(T(-0.0)) == T(-0.0)
-        @test PureLibm.musl_cbrt(T(Inf)) == T(Inf)
-        @test PureLibm.musl_cbrt(T(-Inf)) == T(-Inf)
-        @test isnan(PureLibm.musl_cbrt(T(NaN)))
+# for T in [Float32, Float64]
+#     @testset "musl_cbrt(::$T)" begin
+#         # IEC 60559
+#         @test PureLibm.musl_cbrt(T(0.0)) == T(0.0)
+#         @test PureLibm.musl_cbrt(T(-0.0)) == T(-0.0)
+#         @test PureLibm.musl_cbrt(T(Inf)) == T(Inf)
+#         @test PureLibm.musl_cbrt(T(-Inf)) == T(-Inf)
+#         @test isnan(PureLibm.musl_cbrt(T(NaN)))
 
-        # sanity check
-        @test PureLibm.musl_cbrt(T(1.0)) == T(1.0)
-        @test PureLibm.musl_cbrt(T(27.0)) == T(3.0)
-        @test PureLibm.musl_cbrt(T(1000.0)) == T(10.0)
-        @test PureLibm.musl_cbrt(T(-1.0)) == T(-1.0)
-        @test PureLibm.musl_cbrt(T(-1000.0)) == T(-10.0)
-    end
-end
+#         # sanity check
+#         @test PureLibm.musl_cbrt(T(1.0)) == T(1.0)
+#         @test PureLibm.musl_cbrt(T(27.0)) == T(3.0)
+#         @test PureLibm.musl_cbrt(T(1000.0)) == T(10.0)
+#         @test PureLibm.musl_cbrt(T(-1.0)) == T(-1.0)
+#         @test PureLibm.musl_cbrt(T(-1000.0)) == T(-10.0)
+#     end
+# end
