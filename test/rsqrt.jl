6fc8c7e07591de3776fc5a11612bef453b5ead9c
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-26 14:36:00 +0800

    test.rsqrt: add rand tests

--- test/rsqrt.jl
+++ test/rsqrt.jl
@@ -20,7 +20,7 @@ for T in [Float32, Float64]
 
     @testset "cr_rsqrt(random)" begin
         test_x = T[
-            # rand(0.0:eps():1.0, 10)...,
+            rand(0.0:eps():1.0, 10)...,
             # Branch cov
             # ixu < (UInt64(1) << 52) && ixu != 0
             eps(T(0.0)),
