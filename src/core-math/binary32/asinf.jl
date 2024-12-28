6aaaa5e3ef22f6af32c8dea8d77b984214fcb773
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-28 21:58:28 +0800

    asinf: use NTuple for const

--- src/core-math/binary32/asinf.jl
+++ src/core-math/binary32/asinf.jl
@@ -14,24 +14,24 @@ function _asinf_as_special(x::Float32)
 end
 
 
-const CR_ASINF_B = Vector{Float64}([
+const CR_ASINF_B = NTuple{16, Float64}((
     0x1.0000000000005p+0, 0x1.55557aeca105dp-3, 0x1.3314ec3db7d12p-4, 0x1.775738a5a6f92p-5,
     0x1.5d5f7ce1c8538p-8, 0x1.605c6d58740fp-2, -0x1.5728b732d73c6p+1, 0x1.f152170f151ebp+3,
     -0x1.f962ea3ca992ep+5, 0x1.71971e17375ap+7, -0x1.860512b4ba23p+8, 0x1.26a3b8d4bdb14p+9,
     -0x1.36f2ea5698b51p+9, 0x1.b3d722aebfa2ep+8, -0x1.6cf89703b1289p+7, 0x1.1518af6a65e2dp+5
-])
+))
 
-const CR_ASINF_C1 = Vector{Float64}([
+const CR_ASINF_C1 = NTuple{12, Float64}((
     0x1.555555555529cp-3, 0x1.333333337e0ddp-4, 0x1.6db6db3b4465ep-5, 0x1.f1c72e13ac306p-6,
     0x1.6e89cebe06bc4p-6, 0x1.1c6dcf5289094p-6, 0x1.c6dbbcc7c6315p-7, 0x1.8f8dc2615e996p-7,
     0x1.a5833b7bf15e8p-8, 0x1.43f44ace1665cp-6, -0x1.0fb17df881c73p-6, 0x1.07520c026b2d6p-5
-])
+))
 
-const CR_ASINF_C2 = Vector{Float64}([
+const CR_ASINF_C2 = NTuple{12, Float64}((
     0x1.6a09e667f3bcbp+0, 0x1.e2b7dddff2db9p-4, 0x1.b27247ab42dbcp-6, 0x1.02995cc4e0744p-7,
     0x1.5ffb0276ec8eap-9, 0x1.033885a928decp-10, 0x1.911f2be23f8c7p-12, 0x1.4c3c55d2437fdp-13,
     0x1.af477e1d7b461p-15, 0x1.abd6bdff67dcbp-15, -0x1.1717e86d0fa28p-16, 0x1.6ff526de46023p-16
-])
+))
 
 function cr_asinf(x::Float32)
     """Correctly-rounded arc-sine function for Float32.
