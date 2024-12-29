2b2592fc28654ca19317a55d46192c3074454197
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-29 21:33:49 +0800

    atan2f: rename `CR_ATAN2F_CD`

--- src/core-math/binary32/atan2f.jl
+++ src/core-math/binary32/atan2f.jl
@@ -3,7 +3,7 @@
 # CORE-MATH project Copyright (c) 2022 Alexei Sibidanov.
 
 const CR_ATAN2F_CN = [0x1p+0, 0x1.40e0698f94c35p+1, 0x1.248c5da347f0dp+1, 0x1.d873386572976p-1, 0x1.46fa40b20f1dp-3, 0x1.33f5e041eed0fp-7, 0x1.546bbf28667c5p-14]
-const _CR_ATAN2F_CD = [0x1p+0, 0x1.6b8b143a3f6dap+1, 0x1.8421201d18ed5p+1, 0x1.8221d086914ebp+0, 0x1.670657e3a07bap-2, 0x1.0f4951fd1e72dp-5, 0x1.b3874b8798286p-11]
+const CR_ATAN2F_CD = [0x1p+0, 0x1.6b8b143a3f6dap+1, 0x1.8421201d18ed5p+1, 0x1.8221d086914ebp+0, 0x1.670657e3a07bap-2, 0x1.0f4951fd1e72dp-5, 0x1.b3874b8798286p-11]
 const _CR_ATAN2F_C = Vector{Tuple{Float64, Float64}}([
     (0x1p+0, -0x1.8c1dac5492248p-87), (-0x1.5555555555555p-2, -0x1.55553bf3a2abep-56),
     (0x1.999999999999ap-3, -0x1.99deed1ec9071p-57), (-0x1.2492492492492p-3, -0x1.fd99c8d18269ap-58),
@@ -130,7 +130,7 @@ function cr_atan2f(y::Float32, x::Float32)::Float32
     cn0 += z8 * cn4
     z *= sgn[gt + 1]
 
-    cd = _CR_ATAN2F_CD
+    cd = CR_ATAN2F_CD
     cd0 = cd[1] + z2 * cd[2]
     cd2 = cd[3] + z2 * cd[4]
     cd4 = cd[5] + z2 * cd[6]
