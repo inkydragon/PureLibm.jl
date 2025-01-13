88b1b9e1b19e369af5b8680fc8b8aa19ad690a71
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-13 21:38:17 +0800

    test.log1pf: add more coverage tests

--- test/log1p.jl
+++ test/log1p.jl
@@ -16,6 +16,31 @@ for T in [Float32, ]
         @test isnan(PureLibm.cr_log1p(T(NaN)))
 
         # Coverage
+        if Float32 == T
+            # if @unlikely(ax < 0x33000000)  # |x| < 2.9802322f-8
+            @test PureLibm.cr_log1p(prevfloat(T(2.9802322f-8))) == T(2.980232f-8)
+            @test PureLibm.cr_log1p(-prevfloat(T(2.9802322f-8))) == -T(2.980232f-8)
+            # if @likely(ax < 0x3c880000)  # |x| < 0.016601562f0
+            @test PureLibm.cr_log1p(T(2.9802322f-8)) == T(2.9802322f-8)
+            @test PureLibm.cr_log1p(prevfloat(T(0.016601562f0))) == T(0.016465262f0)
+            # if @unlikely((ru & UInt64(0x0fff_ffff)) == 0)
+            @test PureLibm.cr_log1p(T(1.1444135f-5)) == T(1.144407f-5)
+            @test PureLibm.cr_log1p(T(-7.1525557f-7)) == T(-7.152558f-7)
+            # else
+            # if @unlikely(ub != lb)
+            # if @unlikely((tru & UInt64(0x0fff_ffff)) == 0)
+            @test PureLibm.cr_log1p(T(-0x1.247ab0p-6)) == T(-0.018012777f0)
+            @test PureLibm.cr_log1p(T(-0x1.3a415ep-5)) == T(-0.039116416f0)
+            @test PureLibm.cr_log1p(T(0x1.fb035ap-2)) == T(0.40221313f0)
+            @test PureLibm.cr_log1p(T(0.372202f0)) == T(0.31641674f0)
+            @test PureLibm.cr_log1p(T(5.498306f28)) == T(66.17683f0)
+            # elseif (rl + (Lh - trf)) == 0
+            @test PureLibm.cr_log1p(T(0x1.b7fd86p-4)) == T(0.10203255f0)
+            @test PureLibm.cr_log1p(T(-0x1.3a415ep-5)) == T(-0.039116416f0)
+            @test PureLibm.cr_log1p(T(0x1.43c7e2p-6)) == T(0.019569278f0)
+        else
+            nothing
+        end
     end
 end
 
