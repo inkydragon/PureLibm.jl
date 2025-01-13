83f51d7381ace9331ee97359b22bceda1a591d96
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-13 15:39:34 +0800

    test.expf: add more coverage tests

--- test/exp.jl
+++ test/exp.jl
@@ -9,6 +9,20 @@ for T in [Float32, ]
         @test PureLibm.cr_exp(T(Inf)) == T(Inf)
 
         @test isnan(PureLibm.cr_exp(T(NaN)))
+
+        # Coverage
+        @test PureLibm.cr_exp(T(1)) == T(Base.MathConstants.e)
+        @test PureLibm.cr_exp(T(-1)) == 1/T(Base.MathConstants.e)
+        if Float32 == T
+            # NOTE: for f32 (-103.27881f0, 88.72283f0)
+            @test PureLibm.cr_exp(T(-104)) == T(0)
+            @test PureLibm.cr_exp(T(89)) == T(Inf)
+            # if @unlikely(ub != lb)
+            @test PureLibm.cr_exp(T(0.010941569f0)) == T(1.0110017f0)
+            @test PureLibm.cr_exp(T(-2.613688f-5)) == T(0.9999739f0)
+        else
+            nothing
+        end
     end
 end
 
