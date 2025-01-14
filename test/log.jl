191d352f1dc93a507d167152fb2c843a40758237
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-14 14:02:10 +0800

    test.logf: add coverage tests

--- test/log.jl
+++ test/log.jl
@@ -17,7 +17,17 @@ for T in [Float32, ]
 
         # Coverage
         if Float32 == T
-            # @test PureLibm.cr_log(prevfloat(T(2.9802322f-8))) == T(2.980232f-8)
+            # subnormal
+            @test PureLibm.cr_log(T(0x1p-128)) == T(-88.72284f0)
+            @test PureLibm.cr_log(T(0x1p-134)) == T(-92.88172f0)
+            @test PureLibm.cr_log(T(0x1p-142)) == T(-98.4269f0)
+            # normal path
+            @test PureLibm.cr_log(T(Base.MathConstants.e)) == T(0.99999994f0)
+            @test PureLibm.cr_log(1/T(Base.MathConstants.e)) == T(-1.0f0)
+            # acc path:  if @unlikely(ub != lb)
+            @test PureLibm.cr_log(T(3.2283728f-37)) == T(-84.023674f0)
+            # if @unlikely(abs(x - 1.0) < 0x1p-10)
+            @test PureLibm.cr_log(T(0.999856f0)) == T(-0.0001440152f0)
         end
     end
 end
@@ -33,5 +43,5 @@ if "cr_log" in CheckExhaustive
         test_float_range(log, PureLibm.cr_log, lo=pos_range.lo, hi=pos_range.hi, bigfloat=true)
     end
 end
-# ~ 
+# ~ 27s / 
 # ENV["PURELIBM_CHECK_EXHAUSTIVE"] = "cr_log.fast,cr_log"
