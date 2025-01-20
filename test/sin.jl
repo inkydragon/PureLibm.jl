a9070607b99645fbd736a1a5d6067a9a6136c34d
Chengyu HAN <cyhan.dev@outlook.com>
2025-01-20 22:41:05 +0800

    test.sin: add Coverage tests

--- test/sin.jl
+++ test/sin.jl
@@ -16,7 +16,36 @@ for T in [Float32, ]
         @test PureLibm.cr_sin(T(pi)/2) == T(1.0f0)
         @test PureLibm.cr_sin(T(pi)*3/4) ≈ sqrt(T(2))/2
         @test PureLibm.cr_sin(T(pi)) == T(-8.742278f-8)
+    end
 
+    # Coverage test
+    @testset "cr_sin(random)" begin
+        test_x = T[
+            eps(T(0.0)),
+            # -pi~pi
+            rand_float(Float32(0.0), Float32(pi), 10)...,
+            rand_float(-Float32(0.0), -Float32(pi), 10)...,
+            # 0x66000000 < ax < 0x73000000
+            rand_float(0x33000000, 0x39800000, 4)...,
+            # big input > 0x1p+26
+            #   _sinf_rbig:  if s < 64
+            rand_float(Float32(0x1p+26), Float32(0x1p+84), 4)...,
+            #   _sinf_rbig:  elseif s == 64
+            rand_float(Float32(0x1p+84), Float32(0x1p+85), 4)...,
+            #   _sinf_rbig:  s > 64
+            rand_float(Float32(0x1p+85), typemax(Float32), 4)...,
+            # Special cases: _sinf_database
+            9830.398f0,
+            0.72992426f0,
+            1.3086903f0,
+            9.424778f0,
+        ]
+        @testset "cr_sin($x)" for x in test_x
+            # Test against system libm
+            @test PureLibm.cr_sin(x) ≈ sin(x)
+            # Test against MPFR
+            @test PureLibm.cr_sin(x) === T(sin(BigFloat(x)))
+        end
     end
 end
 
