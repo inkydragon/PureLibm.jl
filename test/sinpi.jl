8c97a7ea46fd34eba49258a1d223bb286b53e9ee
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-14 11:14:25 +0800

    test.sinfpi: add more tests

--- test/sinpi.jl
+++ test/sinpi.jl
@@ -3,50 +3,47 @@
 for T in [Float32, ]
     @testset "cr_sinpi(::$T)" begin
         # IEC 60559
-        # sin(±0) returns ±0
+        # sinpi(±0) returns ±0
         @test PureLibm.cr_sinpi(T(0)) == T(0)
         @test PureLibm.cr_sinpi(-T(0)) == -T(0)
-        # sin(±∞) returns a NaN
+        # sinpi(±n) returns ±0, for positive integers n.
+        for n in 1:10
+            @test PureLibm.cr_sinpi(T(n)) == T(0)
+            @test PureLibm.cr_sinpi(-T(n)) == -T(0)
+        end
+        # sinpi(±∞) returns a NaN
         @test isnan(PureLibm.cr_sinpi(T(Inf)))
         @test isnan(PureLibm.cr_sinpi(T(-Inf)))
 
         # sanity check
         @test isnan(PureLibm.cr_sinpi(T(NaN)))
-        @test PureLibm.cr_sinpi(T(1)/4) ≈ sqrt(T(2))/2
+        @test PureLibm.cr_sinpi(T(1)/4) == sqrt(T(2))/2
         @test PureLibm.cr_sinpi(T(1)/2) == T(1)
-        @test PureLibm.cr_sinpi(T(1)*3/4) ≈ sqrt(T(2))/2
+        @test PureLibm.cr_sinpi(T(1)*3/4) == sqrt(T(2))/2
         @test PureLibm.cr_sinpi(T(1)) == T(0)
     end
 
-    # # Coverage test
-    # @testset "cr_sinpi(random)" begin
-    #     test_x = T[
-    #         eps(T(0.0)),
-    #         # -pi~pi
-    #         rand_float(Float32(0.0), Float32(pi), 10)...,
-    #         rand_float(-Float32(0.0), -Float32(pi), 10)...,
-    #         # 0x66000000 < ax < 0x73000000
-    #         rand_float(0x33000000, 0x39800000, 4)...,
-    #         # big input > 0x1p+26
-    #         #   _sinf_rbig:  if s < 64
-    #         rand_float(Float32(0x1p+26), Float32(0x1p+84), 4)...,
-    #         #   _sinf_rbig:  elseif s == 64
-    #         rand_float(Float32(0x1p+84), Float32(0x1p+85), 4)...,
-    #         #   _sinf_rbig:  s > 64
-    #         rand_float(Float32(0x1p+85), typemax(Float32), 4)...,
-    #         # Special cases: _sinf_database
-    #         9830.398f0,
-    #         0.72992426f0,
-    #         1.3086903f0,
-    #         9.424778f0,
-    #     ]
-    #     @testset "cr_sinpi($x)" for x in test_x
-    #         # Test against system libm
-    #         @test PureLibm.cr_sinpi(x) ≈ sin(x)
-    #         # Test against MPFR
-    #         @test PureLibm.cr_sinpi(x) === T(sin(BigFloat(x)))
-    #     end
-    # end
+    # Coverage test
+    @testset "cr_sinpi(random)" begin
+        test_x = T[
+            eps(T(0.0)),
+            # -2~2
+            rand_float(Float32(0), Float32(2), 10)...,
+            rand_float(-Float32(0), -Float32(2), 10)...,
+            # s < 0  # |x| >= 0x1p+17
+            0x1p+17, 0x1p+22,
+            rand_float(Float32(0x1p+17), Float32(0x1p+23), 8)...,
+            # s < 0 && s < -6  # |x| >= 0x1p+23
+            0x1p+23,
+            rand_float(Float32(0x1p+23), prevfloat(Inf32), 8)...,
+        ]
+        @testset "cr_sinpi($x)" for x in test_x
+            # Test against system libm
+            @test PureLibm.cr_sinpi(x) ≈ sinpi(x)
+            # Test against MPFR
+            @test PureLibm.cr_sinpi(x) === T(sinpi(BigFloat(x)))
+        end
+    end
 end
 
 pos_range = (lo=Float32(0.0), hi=prevfloat(Float32(Inf)))
