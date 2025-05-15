3ca0eeb9bdbaa2a4aace7a7dbfe1d82f203d0595
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-15 18:32:53 +0800

    test.cospi: add IEC 60559

--- test/cospi.jl
+++ test/cospi.jl
@@ -1,8 +1,19 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0
 
-for T in [Float32, ]
+for T in [Float32]
     @testset "cr_cospi(::$T)" begin
         # IEC 60559
+        # cospi(±0) returns 1
+        @test PureLibm.cr_cospi(T(0.0)) == T(1.0)
+        @test PureLibm.cr_cospi(T(-0.0)) == T(1.0)
+        # cospi(n + 1/2) returns +0, for integers n
+        for n in rand(1:10^6, 8)
+            @test PureLibm.cr_cospi(n + T(0.5)) == T(0.0)
+            @test PureLibm.cr_cospi(-n + T(0.5)) == T(0.0)
+        end
+        # cospi(±∞) returns a NaN and raises the "invalid" floating-point exception
+        @test isnan(PureLibm.cr_cospi(T(Inf)))
+        @test isnan(PureLibm.cr_cospi(T(-Inf)))
 
         # sanity check
         @test isnan(PureLibm.cr_cospi(T(NaN)))
