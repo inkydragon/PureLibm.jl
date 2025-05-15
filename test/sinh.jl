065d58590be892bf40e649e0df5d37c65107848c
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-15 15:11:37 +0800

    test.sinh: add branch coverage tests

--- test/sinh.jl
+++ test/sinh.jl
@@ -34,7 +34,23 @@ for T in [Float32, ]
             eps(T(0.0)),
             # [0, 90]
             rand_float(T(0), T(90), 16)...,
+
+            # branch coverage
+            # ux == 0x74250bfe
+            0.0005589425,
+            # (ux <= 0x74250bfe) && !(ux < 0x66000000)
+            # [0x1p-24, 0x1.250bfep-11]
+            rand_float(T(0x1p-24), T(0x1.250bfep-11), 8)...,
+            # (ux < 0x7c000000) && !(ux <= 0x74250bfe)
+            # [0x1.250bfep-11, 0.125]
+            rand_float(T(0x1.250bfep-11), T(0.125), 8)...,
+            # (ub != lb)
+            0.2567545,
+            0.3000602,
+            3.00006,
+            3.9815507,
         ]
+        test_x = [test_x..., -test_x...]
         @testset "cr_sinh($x)" for x in test_x
             # Test against system libm
             @test PureLibm.cr_sinh(x) ≈ sinh(x)
