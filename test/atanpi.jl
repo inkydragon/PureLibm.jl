be485c8188d80f86684f4f75ee54a0b71dc3f672
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-17 21:27:10 +0800

    test.atanpi: fmt

--- test/atanpi.jl
+++ test/atanpi.jl
@@ -3,7 +3,7 @@
 # ref
 _atanpi(x::T) where {T<:AbstractFloat} = T(atan(x) / pi)
 
-for T in [Float32, ]
+for T in [Float32]
     @testset "cr_atanpi($T)" begin
         # IEC 60559
         @test isnan(PureLibm.cr_atanpi(T(NaN)))
@@ -11,12 +11,12 @@ for T in [Float32, ]
         @test PureLibm.cr_atanpi(T(0.0)) == T(0.0)
         @test PureLibm.cr_atanpi(T(-0.0)) == T(-0.0)
         # atanpi(±∞) returns ±1/2
-        @test PureLibm.cr_atanpi(T(Inf)) == 1/2
-        @test PureLibm.cr_atanpi(T(-Inf)) == -1/2
+        @test PureLibm.cr_atanpi(T(Inf)) == 1 / 2
+        @test PureLibm.cr_atanpi(T(-Inf)) == -1 / 2
 
         # sanity check
-        @test PureLibm.cr_atanpi(T(1)) == T(1/4)
-        @test PureLibm.cr_atanpi(-T(1)) == -T(1/4)
+        @test PureLibm.cr_atanpi(T(1)) == T(1) / 4
+        @test PureLibm.cr_atanpi(-T(1)) == -T(1) / 4
     end
 
     @testset "cr_atanpi(random)" begin
@@ -56,14 +56,18 @@ pos_range = (lo=Float32(0.0), hi=prevfloat(Float32(Inf)))
 neg_range = (lo=Float32(-0.0), hi=nextfloat(Float32(-Inf)))
 if "cr_atanpi.fast" in CheckExhaustive
     @testset "cr_atanpi-exhaustive.fast" begin
-        test_float_range(_atanpi, PureLibm.cr_atanpi, lo=pos_range.lo, hi=pos_range.hi)
-        test_float_range(_atanpi, PureLibm.cr_atanpi, lo=neg_range.lo, hi=neg_range.hi)
+        test_float_range(_atanpi, PureLibm.cr_atanpi; lo=pos_range.lo, hi=pos_range.hi)
+        test_float_range(_atanpi, PureLibm.cr_atanpi; lo=neg_range.lo, hi=neg_range.hi)
     end
 end
 if "cr_atanpi" in CheckExhaustive
     @testset "cr_atanpi-exhaustive" begin
-        test_float_range(_atanpi, PureLibm.cr_atanpi, lo=pos_range.lo, hi=pos_range.hi, bigfloat=true)
-        test_float_range(_atanpi, PureLibm.cr_atanpi, lo=neg_range.lo, hi=neg_range.hi, bigfloat=true)
+        test_float_range(
+            _atanpi, PureLibm.cr_atanpi; lo=pos_range.lo, hi=pos_range.hi, bigfloat=true
+        )
+        test_float_range(
+            _atanpi, PureLibm.cr_atanpi; lo=neg_range.lo, hi=neg_range.hi, bigfloat=true
+        )
     end
 end
 # ENV["PURELIBM_CHECK_EXHAUSTIVE"] = "cr_atanpi.fast,cr_atanpi"
