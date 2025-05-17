77de2b0057be5b51d2351fd4415115c81ca79915
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-17 21:23:50 +0800

    test.atanpi: add Branch Coverage

--- test/atanpi.jl
+++ test/atanpi.jl
@@ -22,7 +22,25 @@ for T in [Float32, ]
     @testset "cr_atanpi(random)" begin
         test_x = T[
             eps(T(0.0)),
- 
+            # Note: atan(6e15) == atan(Inf)
+            rand_float(T(0.0), T(6e15), 64)...,
+
+            # Branch Coverage
+            # abs(x) >= Float32(0x1.45f306p+124)
+            rand_float(T(0x1.45f306p+124), T(prevfloat(Float32(Inf))), 4)...,
+            # !(abs(x) >= Float32(0x1.45f306p+124))
+            # |x| >= 2^25 && |x| < 0x1.45f306p+124
+            rand_float(T(0x1p25), T(0x1.45f306p+124), 4)...,
+            # (e < (127 - 13)) && !(e < (127 - 25))
+            # 2^-25 <= |x| < 2^-13
+            rand_float(T(0x1p-25), T(0x1p-13), 4)...,
+            # hard to round cases
+            0x1.44cfbap+0,
+            0x1.d26a62p-1,
+            0x1p+0,
+            # !(e > (127 + 24)) && !(e < (127 - 13))
+            # 2^-13 <= |x| < 2^25
+            rand_float(T(0x1p-13), T(0x1p25), 4)...,
         ]
         test_x = [test_x..., -test_x...]
         @testset "cr_atanpi($x)" for x in test_x
