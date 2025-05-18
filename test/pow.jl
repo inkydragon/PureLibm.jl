60122860c4ef941dec5902828632f6e25c17ddd7
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-18 22:18:50 +0800

    Create pow.jl

---
+++ test/pow.jl
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+for T in (Float32, )
+    @testset "cr_hypot($T)" begin
+        #= IEC 60559 =#
+        # — pow(±0, y) returns ±∞ and raises the "divide-by-zero" floating-point exception
+        #   for y an odd integer < 0
+        # — pow(±0, y) returns +∞ and raises the "divide-by-zero" floating-point exception
+        #   for y < 0, finite, and not an odd integer
+        # — pow(±0, −∞) returns +∞
+        # — pow(±0, y) returns ±0 for y an odd integer > 0
+        # — pow(±0, y) returns +0 for y > 0 and not an odd integer
+        # — pow(−1, ±∞) returns 1
+        # — pow(+1, y) returns 1 for any y, even a NaN
+        # — pow(x, ±0) returns 1 for any x, even a NaN
+        # — pow(x, y) returns a NaN and raises the "invalid" floating-point exception
+        #   for finite x < 0 and finite non-integer y
+        # — pow(x, −∞) returns +∞ for |x| < 1
+        # — pow(x, −∞) returns +0 for |x| > 1
+        # — pow(x, +∞) returns +0 for |x| < 1
+        # — pow(x, +∞) returns +∞ for |x| > 1
+        # — pow(−∞, y) returns −0 for y an odd integer < 0
+        # — pow(−∞, y) returns +0 for y < 0 and not an odd integer
+        # — pow(−∞, y) returns −∞ for y an odd integer > 0
+        # — pow(−∞, y) returns +∞ for y > 0 and not an odd integer
+        # — pow(+∞, y) returns +0 for y < 0
+        # — pow(+∞, y) returns +∞ for y > 0
+    end
+end
