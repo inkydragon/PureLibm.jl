be16864f1082d57c8745bc3103ee5c4d401f5574
Chengyu Han <cyhan.dev@outlook.com>
2025-05-19 00:25:42 +0800

    doc: add ref to Std C23 § F.10 (#60)
    * doc: add ref to Std C23 § F.10.1
    
    * doc: add ref to Std C23 § F.10.2, fix typo
    
    * doc: add ref to Std C23 § F.10.3
    
    * doc: add ref to Std C23 § F.10.4
    
    * doc: add ref to Std C23 § F.10.5
    
    * doc: remove un-impl fucntions

--- src/doc.jl
+++ src/doc.jl
@@ -12,6 +12,7 @@ Returns the arc-cosine of `x` in ranges `[0, pi]`.
 
 # Reference
 - [DLMF: §4.23.2](https://dlmf.nist.gov/4.23#E2)
+- C23 F.10.1.1
 """
 cr_acos
 
@@ -27,6 +28,7 @@ Returns the arc-sine of `x` in ranges `[-pi/2, pi/2]`.
 
 # Reference
 - [DLMF: §4.23.1](https://dlmf.nist.gov/4.23#E1)
+- C23 F.10.1.2
 """
 cr_asin
 
@@ -42,8 +44,16 @@ Returns the arc-tangent of `x` in ranges `[-pi/2, pi/2]`.
 
 # Reference
 - [DLMF: §4.23.3](https://dlmf.nist.gov/4.23#E3)
+- C23 F.10.1.3
 """
 cr_atan
+
+"""
+    cr_atan2(x, y)
+
+# Reference
+- C23 F.10.1.4
+"""
 # cr_atan2
 
 """
@@ -58,6 +68,7 @@ Returns the cosine of `x` in ranges `[-1, 1]`.
 
 # Reference
 - [DLMF: §4.14.2](https://dlmf.nist.gov/4.14#E2)
+- C23 F.10.1.5
 """
 cr_cos
 
@@ -73,6 +84,7 @@ Returns the sine of `x` in ranges `[-1, 1]`.
 
 # Reference
 - [DLMF: §4.14.1](https://dlmf.nist.gov/4.14#E1)
+- C23 F.10.1.6
 """
 cr_sin
 
@@ -95,6 +107,7 @@ Returns the tangent of `x` in ranges `[-∞, ∞]`.
 
 # Reference
 - [DLMF: §4.14.4](https://dlmf.nist.gov/4.14#E4)
+- C23 F.10.1.7
 """
 cr_tan
 
@@ -102,13 +115,19 @@ cr_tan
     cr_acospi(x)
 
 Compute the inverse cosine of `x` divided by `π`.
-"""
 
+# Reference
+- C23 F.10.1.8
+"""
 cr_acospi
+
 """
     cr_asinpi(x)
 
 Compute the inverse sine of `x` divided by `π`.
+
+# Reference
+- C23 F.10.1.9
 """
 cr_asinpi
 
@@ -116,9 +135,19 @@ cr_asinpi
     cr_atanpi(x)
 
 Compute the inverse tangent of `x` divided by `π`.
+
+# Reference
+- C23 F.10.1.10
 """
 cr_atanpi
-# atan2pi
+
+"""
+    cr_atan2pi(x)
+
+# Reference
+- C23 F.10.1.11
+"""
+# cr_atan2pi
 
 """
     cr_cospi(x)
@@ -130,6 +159,9 @@ Returns the cosine of `x` in ranges `[-1, 1]`.
 - Returns `+0` if `x` is `n + 1/2`, for integers `n`
 - Returns `NaN` if `x` is `±∞`
 - Returns `NaN` if `x` is `NaN`
+
+# Reference
+- C23 F.10.1.12
 """
 cr_cospi
 
@@ -143,6 +175,9 @@ Returns the sine of `x` in ranges `[-1, 1]`.
 - Returns `±0` if `x` is `±n`, for positive integers `n`
 - Returns `NaN` if `x` is `±∞`
 - Returns `NaN` if `x` is `NaN`
+
+# Reference
+- C23 F.10.1.13
 """
 cr_sinpi
 
@@ -159,6 +194,9 @@ Returns the tangent of `x` in ranges `[-∞, ∞]`.
 - Returns `-∞` if `x` is `n + 1/2`, for odd integers `n`
 - Returns `NaN` if `x` is `±∞`
 - Returns `NaN` if `x` is `NaN`
+
+# Reference
+- C23 F.10.1.14
 """
 cr_tanpi
 
@@ -177,6 +215,7 @@ Returns the inverse hyperbolic cosine of `x`.
 
 # Reference
 - [DLMF: §4.37.2](https://dlmf.nist.gov/4.37#E2)
+- C23 F.10.2.1
 """
 cr_acosh
 
@@ -192,6 +231,7 @@ Returns the inverse hyperbolic sine of `x`.
 
 # Reference
 - [DLMF: §4.37.1](https://dlmf.nist.gov/4.37#E1)
+- C23 F.10.2.2
 """
 cr_asinh
 
@@ -208,6 +248,7 @@ Returns the inverse hyperbolic tangent of `x`.
 
 # Reference
 - [DLMF: §4.37.3](https://dlmf.nist.gov/4.37#E3)
+- C23 F.10.2.3
 """
 cr_atanh
 
@@ -223,6 +264,7 @@ Returns the hyperbolic cosine of `x`.
 
 # Reference
 - [DLMF: §4.28.2](https://dlmf.nist.gov/4.28#E2)
+- C23 F.10.2.4
 """
 cr_cosh
 
@@ -238,6 +280,7 @@ Returns the hyperbolic sine of `x`.
 
 # Reference
 - [DLMF: §4.28.1](https://dlmf.nist.gov/4.28#E1)
+- C23 F.10.2.5
 """
 cr_sinh
 
@@ -248,11 +291,12 @@ Compute the hyperbolic tangent of `x` in radians.
 
 Returns the hyperbolic tangent of `x`.
 - Returns `±0` if `x` is `±0`
-- Returns `±∞` if `x` is `±1`
+- Returns `±1` if `x` is `±∞`
 - Returns `NaN` if `x` is `NaN`
 
 # Reference
 - [DLMF: §4.28.4](https://dlmf.nist.gov/4.28#E4)
+- C23 F.10.2.6
 """
 cr_tanh
 
@@ -262,6 +306,9 @@ cr_tanh
     cr_exp(x)
 
 Compute natural exponential of `x`.
+
+# Reference
+- C23 F.10.3.1
 """
 cr_exp(x::Float32) = cr_expf(x)
 
@@ -269,22 +316,45 @@ cr_exp(x::Float32) = cr_expf(x)
     cr_exp10(x)
 
 Compute `10^x` of `x`.
+
+# Reference
+- C23 F.10.3.2
 """
 cr_exp10(x::Float32) = cr_exp10f(x)
-# exp10m1
+
+"""
+    cr_exp10m1(x)
+
+# Reference
+- C23 F.10.3.3
+"""
+# cr_exp10m1
 
 """
     cr_exp2(x)
 
 Compute `2^x` of `x`.
+
+# Reference
+- C23 F.10.3.4
 """
 cr_exp2(x::Float32) = cr_exp2f(x)
-# exp2m1
+
+"""
+    cr_exp2m1(x)
+
+# Reference
+- C23 F.10.3.5
+"""
+# cr_exp2m1
 
 """
     cr_expm1(x)
 
 Compute `exp(x) - 1` of `x`.
+
+# Reference
+- C23 F.10.3.6
 """
 cr_expm1(x::Float32) = cr_expm1f(x)
 
@@ -292,6 +362,9 @@ cr_expm1(x::Float32) = cr_expm1f(x)
     cr_log(x)
 
 Compute natural logarithm of `x`.
+
+# Reference
+- C23 F.10.3.11
 """
 cr_log(x::Float32) = cr_logf(x)
 
@@ -299,25 +372,48 @@ cr_log(x::Float32) = cr_logf(x)
     cr_log10(x)
 
 Compute base 10 logarithm of `x`.
+
+# Reference
+- C23 F.10.3.12
 """
 cr_log10
-# log10p1
+
+"""
+    cr_log10p1(x)
+
+# Reference
+- C23 F.10.3.13
+"""
+# cr_log10p1
 
 """
     cr_log1p(x)
 
 Compute biased argument natural logarithm `log(1+x)` of `x`.
+
+# Reference
+- C23 F.10.3.14
 """
 cr_log1p(x::Float32) = cr_log1pf(x)
-# logp1
+const cr_logp1 = cr_log1p
 
 """
     cr_log2(x)
 
 Compute natural logarithm of `x`.
+
+# Reference
+- C23 F.10.3.15
 """
 cr_log2
-# log2p1
+
+"""
+    cr_log2p1(x)
+
+# Reference
+- C23 F.10.3.16
+"""
+# cr_log2p1
 
 
 #= Power and Absolute-value =#
@@ -325,12 +421,28 @@ cr_log2
     cr_cbrt(x)
 
 Compute cubic root of `x`.
+
+# Reference
+- C23 F.10.4.1
 """
 cr_cbrt
 # compoundn
-# hypot
 
-# pow
+"""
+    cr_hypot(x)
+
+# Reference
+- C23 F.10.4.4
+"""
+# cr_hypot
+
+"""
+    cr_pow(x)
+
+# Reference
+- C23 F.10.4.5
+"""
+# cr_pow
 # pown
 # powr
 # rootn
@@ -339,19 +451,52 @@ cr_cbrt
     cr_rsqrt(x)
 
 Computes the reciprocal square root of `x`.
+
+# Reference
+- C23 F.10.4.9
 """
 cr_rsqrt(x::Float32) = cr_rsqrtf(x)
-# sqrt
+
+"""
+    cr_sqrt(x)
+
+# Reference
+- C23 F.10.4.10
+"""
+# cr_sqrt
 
 
 #= Error and gamma =#
-# erf
-# erfc
+"""
+    cr_erf(x)
 
-# lgamma
+# Reference
+- C23 F.10.5.1
+"""
+# cr_erf
+
+"""
+    cr_erfc(x)
+
+# Reference
+- C23 F.10.5.2
+"""
+# cr_erfc
+
+"""
+    cr_lgamma(x)
+
+# Reference
+- C23 F.10.5.3
+"""
+# cr_lgamma
+
 """
     cr_tgamma(x)
 
 Computes the true gamma function of `x`.
+    
+# Reference
+- C23 F.10.5.4
 """
 cr_tgamma(x::Float32) = cr_tgammaf(x)
