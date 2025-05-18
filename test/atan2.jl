356474c83e44ef9a16ffcb48bbfbf79d85153d80
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-18 21:50:15 +0800

    atan2: add IEC 60559

---
+++ test/atan2.jl
@@ -0,0 +1,34 @@
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+for T in (Float32, )
+    @testset "cr_atan2($T)" begin
+        # IEC 60559
+        # atan2(±0, −0) returns ±π
+        # atan2(±0, +0) returns ±0
+        # atan2(±0, x) returns ±π for x < 0
+        # atan2(±0, x) returns ±0 for x > 0
+        # atan2(y, ±0) returns -π/2 for y < 0
+        # atan2(y, ±0) returns π/2 for y > 0
+        # atan2(±y, −∞) returns ±π for finite y > 0
+        # atan2(±y, +∞) returns ±0 for finite y > 0
+        # atan2(±∞, x) returns ±π/2 for finite x
+        # atan2(±∞, −∞) returns ±3π/4
+        # atan2(±∞, +∞) returns ±π/4
+
+        # sanity check
+    end
+end
+
+# if "cr_atan2.fast" in CheckExhaustive
+#     @testset "cr_atan2-exhaustive.fast" begin
+#         test_float_range(atan2, PureLibm.cr_atan2, lo=Float32(0.0), hi=Float32(4pi))
+#         test_float_range(atan2, PureLibm.cr_atan2, lo=Float32(-0.0), hi=Float32(-4pi))
+#     end
+# end
+# if "cr_atan2" in CheckExhaustive
+#     @testset "cr_atan2-exhaustive" begin
+#         test_float_range(atan2, PureLibm.cr_atan2, lo=Float32(0.0), hi=Float32(4pi), bigfloat=true)
+#         test_float_range(atan2, PureLibm.cr_atan2, lo=Float32(-0.0), hi=Float32(-4pi), bigfloat=true)
+#     end
+# end
+# ENV["PURELIBM_CHECK_EXHAUSTIVE"] = "cr_atan2.fast,cr_atan2"
