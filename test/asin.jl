d2e5bc2b29a28e35bb2775087e79ab875cc8aab6
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-18 21:47:46 +0800

    asin

--- test/asin.jl
+++ test/asin.jl
@@ -6,7 +6,8 @@ for T in [Float32, ]
         # asin(±0) returns +0
         @test PureLibm.cr_asin(T(0.0)) == T(0.0)
         @test PureLibm.cr_asin(T(-0.0)) == T(-0.0)
-        # asin(x), |x| > 1 returns a NaN
+        # asin(x) returns a NaN and raises the "invalid" floating-point exception
+        #   for |x| > 1
         @test isnan(PureLibm.cr_asin(nextfloat(T(1))))
         @test isnan(PureLibm.cr_asin(prevfloat(T(-1))))
         @test isnan(PureLibm.cr_asin(T(2)))
