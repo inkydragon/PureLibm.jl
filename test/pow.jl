a979856404994ef9ea881b05a601e5cdc8ece26e
Chengyu HAN <cyhan.dev@outlook.com>
2025-05-19 17:13:31 +0800

    test: add cr_pow

--- test/pow.jl
+++ test/pow.jl
@@ -3,14 +3,21 @@
 for T in (Float32, )
     @testset "cr_hypot($T)" begin
         #= IEC 60559 =#
+        @test isnan(PureLibm.cr_hypot(T(0), T(NaN)))
+        @test isnan(PureLibm.cr_hypot(T(NaN), T(0)))
+        @test isnan(PureLibm.cr_hypot(T(NaN), T(NaN)))
         # — pow(±0, y) returns ±∞ and raises the "divide-by-zero" floating-point exception
         #   for y an odd integer < 0
         # — pow(±0, y) returns +∞ and raises the "divide-by-zero" floating-point exception
         #   for y < 0, finite, and not an odd integer
         # — pow(±0, −∞) returns +∞
+        @test PureLibm.cr_hypot(T(0), -T(Inf)) == T(Inf)
+        @test PureLibm.cr_hypot(-T(0), -T(Inf)) == T(Inf)
         # — pow(±0, y) returns ±0 for y an odd integer > 0
         # — pow(±0, y) returns +0 for y > 0 and not an odd integer
         # — pow(−1, ±∞) returns 1
+        @test PureLibm.cr_hypot(-T(1), T(Inf)) == T(1)
+        @test PureLibm.cr_hypot(-T(1), -T(Inf)) == T(1)
         # — pow(+1, y) returns 1 for any y, even a NaN
         # — pow(x, ±0) returns 1 for any x, even a NaN
         # — pow(x, y) returns a NaN and raises the "invalid" floating-point exception
