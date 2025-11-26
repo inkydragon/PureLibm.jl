ec6d6c3254a3b786710196eb3af9bbcdcba1bb85
Chengyu HAN <cyhan.dev@outlook.com>
2025-11-27 03:16:00 +0800

    exp10m1f.doc: add comments, examples

--- src/core-math/binary32/exp10m1f.jl
+++ src/core-math/binary32/exp10m1f.jl
@@ -60,8 +60,28 @@ const CR_EXP10M1F_CP_9 = NTuple{9,Float64}([
 
 Correctly-rounded base-10 exponent function biased by 1 for `Float32`.
 
+# Examples
+```jldoctest
+julia> PureLibm.cr_exp10m1.(Float32[0.0, 1, 2, 3])
+4-element Vector{Float32}:
+   0.0
+   9.0
+  99.0
+ 999.0
+
+julia> PureLibm.cr_exp10m1.(Float32[38, 39, 40, Inf])
+4-element Vector{Float32}:
+  1.0f38
+ Inf
+ Inf
+ Inf
+
+julia> PureLibm.cr_exp10m1(-Inf32)
+-1.0f0
+```
+
 # Reference
-- core-math/src/binary32/exp10m1/exp10m1f.c
+- [core-math/src/binary32/exp10m1/exp10m1f.c](https://github.com/inkydragon/core-math/blob/6616e78eb7083f63bdb3875204c39849cb2db7c8/src/binary32/exp10m1/exp10m1f.c)
 """
 cr_exp10m1(x::Float32) = cr_exp10m1f(x)
 
@@ -71,7 +91,7 @@ function cr_exp10m1f(x::Float32)::Float32
     ax = tu & (typemax(UInt32) >> 1)
 
     if @unlikely(tu > 0xc0f0d2f1)
-        # x < -7.52575
+        # x < -7.5257497f0
         if ax > (UInt32(0xff) << 23)
             # exp10m1(NaN) = NaN
             return x + x
@@ -84,7 +104,7 @@ function cr_exp10m1f(x::Float32)::Float32
             return CR_EXP10M1F_Q[2][1] + CR_EXP10M1F_Q[2][2]
         end
     elseif @unlikely(ax > 0x421a209a)
-        # x > 38.5318
+        # |x| > 38.531837f0
         if ax >= (UInt32(0xff) << 23)
             # exp10m1(+Inf) = +Inf
             # exp10m1(NaN) = NaN
@@ -94,24 +114,25 @@ function cr_exp10m1f(x::Float32)::Float32
         # errno = ERANGE  # overflow
         return CR_EXP10M1F_Q[1][1] + CR_EXP10M1F_Q[1][2]
     elseif @unlikely(ax < 0x3d89c604)
-        # |x| < 0.1549/log(10)
+        # |x| < 0.1549/log(10) == 0.067272216f0
         z2 = z * z
         r = 0.0
         if @unlikely(ax < 0x3d1622fb)
-            # |x| < 8.44e-2/log(10)
+            # |x| < 8.44e-2/log(10) == 0.036654454f0
             if @unlikely(ax < 0x3c8b76a3)
-                # |x| < 3.92e-2/log(10)
+                # |x| < 3.92e-2/log(10) == 0.017024344f0
                 if @unlikely(ax < 0x3bcced04)
-                    # |x| < 1.44e-2/log(10)
+                    # |x| < 1.44e-2/log(10) == 0.0062538404f0
                     if @unlikely(ax < 0x3acf33eb)
-                        # |x| < 3.64e-3/log(10)
+                        # |x| < 3.64e-3/log(10) == 0.001580832f0
                         if @unlikely(ax < 0x395a966b)
-                            # |x| < 4.8e-4/log(10)
+                            # |x| < 4.8e-4/log(10) == 0.00020846135f0
                             if @unlikely(ax < 0x36fe4a4b)
-                                # |x| < 1.745e-5/log(10)
+                                # |x| < 1.745e-5/log(10) == 7.5784387f-6
                                 if @unlikely(ax < 0x32407f39)
-                                    # |x| < 2.58e-8/log(10)
+                                    # |x| < 2.58e-8/log(10) == 1.1204798f-8
                                     if @unlikely(ax < 0x245e5bd9)
+                                        # |x| <= 4.8216374f-17
                                         #=
                                             for |x| <= 0x1.bcb7bp-128, exp10m1(x) underflows,
                                             except for rounding away from zero
@@ -120,6 +141,7 @@ function cr_exp10m1f(x::Float32)::Float32
                                         r = 0x1.26bb1bbb55516p+1
                                     else
                                         if @unlikely(tu == 0x2c994b7b)
+                                            # x = 4.3569016f-12
                                             return Float32(0x1.60f974p-37) -
                                                    Float32(0x1p-90)
                                         end
@@ -128,6 +150,7 @@ function cr_exp10m1f(x::Float32)::Float32
                                     end
                                 else
                                     if @unlikely(tu == 0xb6fa215b)
+                                        # x = -7.4544637f-6
                                         return -Float32(0x1.1ff87ep-16) + Float32(0x1p-68)
                                     end
 
@@ -172,8 +195,8 @@ function cr_exp10m1f(x::Float32)::Float32
 
         return Float32(r)
     else
-        # -7.52575 < x < -0.1549/log(10)
-        # or  0.1549/log(10) < x < 38.5318
+        # -7.5257497f0 < x < -0.1549/log(10)
+        # or  0.067272216f0 == 0.1549/log(10) < x < 38.531837f0
         if @unlikely((tu << 11) == 0)
             k = Int((tu >> 21) - 0x000001fc)
             if k <= 0x0000000b
