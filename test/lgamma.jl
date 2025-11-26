bda4cf121df3644f42be9d415d3631289bcdb151
Chengyu Han <cyhan.dev@outlook.com>
2025-11-26 22:47:23 +0800

    lgammaf: port from Core-Math (#68)
    * lgammaf: port from Core-Math
    
    * test.lgamma: add tests
    
    * test.lgamma: add more sanity check
    
    * lgammaf: bugfix, fix e < 0
    
    * test.lgamma: add rand tests
    
    * test.lgamma: add more Branch cov
    
    * lgammaf: bugfix, fix typo
    
    * test.lgamma: cov test workaround
    
    * test.lgamma: skip SpecialFunctions.lgamma
    
    * lgammaf: add doc and examples
    
    * lgammaf: use const tup
    
    * lgammaf: format
    
    * test.lgamma: add PURELIBM_CHECK_EXHAUSTIVE
    
    * lgammaf: bugfix, include last item
    
    * lgammaf: format tup
    
    * lgammaf: more comments
    
    * test.lgamma: mark Branch cov cases for Float32

--- test/lgamma.jl
+++ test/lgamma.jl
@@ -3,22 +3,102 @@ import SpecialFunctions
 
 for T in (Float32, )
     @testset "cr_lgamma($T)" begin
-        @test_throws ErrorException PureLibm.cr_lgamma(T(1.0))
         # IEC 60559
-        # @test isnan(PureLibm.cr_lgamma(T(NaN)))
-        # # — lgamma(1) returns +0
-        # @test PureLibm.cr_lgamma(T(1)) == T(0.0)
-        # # — lgamma(2) returns +0
-        # @test PureLibm.cr_lgamma(T(2)) == T(0.0)
-        # # — lgamma(x) returns +∞ and raises the "divide-by-zero" floating-point exception
-        # #   for x a negative integer or zero.
-        # @test PureLibm.cr_lgamma(T(0.0)) == T(Inf)
-        # @test PureLibm.cr_lgamma(-T(1)) == T(Inf)
-        # @test PureLibm.cr_lgamma(-T(2)) == T(Inf)
-        # @test PureLibm.cr_lgamma(-T(100)) == T(Inf)
-        # # — lgamma(−∞) returns +∞
-        # @test PureLibm.cr_lgamma(T(-Inf)) == T(Inf)
-        # # — lgamma(+∞) returns +∞
-        # @test PureLibm.cr_lgamma(T(Inf)) == T(Inf)
+        @test isnan(PureLibm.cr_lgamma(T(NaN)))
+        # — lgamma(1) returns +0
+        @test PureLibm.cr_lgamma(T(1)) == T(0.0)
+        # — lgamma(2) returns +0
+        @test PureLibm.cr_lgamma(T(2)) == T(0.0)
+        # — lgamma(x) returns +∞ and raises the "divide-by-zero" floating-point exception
+        #   for x a negative integer or zero.
+        @test PureLibm.cr_lgamma(T(0.0)) == T(Inf)
+        @test PureLibm.cr_lgamma(-T(0.0)) == T(Inf)
+        @test PureLibm.cr_lgamma(-T(1)) == T(Inf)
+        @test PureLibm.cr_lgamma(-T(2)) == T(Inf)
+        @test PureLibm.cr_lgamma(-T(100)) == T(Inf)
+        # — lgamma(−∞) returns +∞
+        @test PureLibm.cr_lgamma(T(-Inf)) == T(Inf)
+        # — lgamma(+∞) returns +∞
+        @test PureLibm.cr_lgamma(T(Inf)) == T(Inf)
+
+        # ---- sanity check
+        # lgamma(x+1) = lgamma(x) + log(x)
+        # lgamma(0.5) = log(π^0.5)
+        @test PureLibm.cr_lgamma(T(0.5)) ≈ T(0.5 * log(π))
+        # lgamma(3/2) = log(1/2 * √π)
+        @test PureLibm.cr_lgamma(T(1.5)) ≈ T(0.5 * log(π) - log(2))
+        # lgamma(5/2) = log(3/4 * √π)
+        @test PureLibm.cr_lgamma(T(2.5)) ≈ T(0.5 * log(π) + log(3) - 2 * log(2))
+        # lgamma(7/2) = log(15/8 * √π)
+        @test PureLibm.cr_lgamma(T(3.5)) ≈ T(0.5 * log(π) + log(15) - 3 * log(2))
+        # lgamma(-1/2) = log(|-2 * √π|)
+        @test PureLibm.cr_lgamma(T(-0.5)) ≈ T(log(2) + 0.5 * log(π))
+        # lgamma(-3/2) = log(|4/3 * √π|)
+        @test PureLibm.cr_lgamma(T(-1.5)) ≈ T(log(4/3) + 0.5 * log(π))
+        # lgamma(-5/2) = log(|-8/15 * √π|)
+        @test PureLibm.cr_lgamma(T(-2.5)) ≈ T(log(8/15) + 0.5 * log(π))
+        #= Γ(z)Γ(1-z) = π / sin(πz) =#
+        # Γ(1/3)Γ(2/3) = 2π/√3
+        @test PureLibm.cr_lgamma(T(1//3)) + PureLibm.cr_lgamma(T(2//3)) ≈ T(log(2π) - log(sqrt(3)))
+        # Γ(1/4)Γ(3/4) = π√2
+        @test PureLibm.cr_lgamma(T(1//4)) + PureLibm.cr_lgamma(T(3//4)) ≈ T(log(π) + log(sqrt(2)))
+    end
+
+    @testset "cr_lgamma(random)" begin
+        test_x = T[
+            eps(T(0.0)),
+            rand_float(T(0.0), T(1.0), 16)...,
+            rand_float(T(1.0), T(2.0), 64)...,
+            rand_float(T(1), T(100), 64)...,
+            1:100...,
+        ]
+        if Float32 == T
+            # Branch cov
+            append!(test_x, T[
+                # if @unlikely(x >= Float32(0x1.895f1cp+121))
+                0x1.895f1cp+121, 5.0f36,
+                # if ax > 1198.0f0
+                rand_float(1198.0f0, 1.048576f6, 8)...,
+
+                # if x < 0.0f0
+                # if @unlikely(tu < 0x40301b93 && tu > 0x402f95c2)
+                rand_float(2.7435155f0, 2.751683f0, 8)...,
+                # elseif @unlikely(tu > 0x401ceccb && tu < 0x401d95ca)
+                rand_float(2.4519527f0, 2.4622674f0, 8)...,
+                # elseif @unlikely(tu > 0x40492009 && tu < 0x404940ef)
+                rand_float(3.1425803f0, 3.1445882f0, 8)...,
+
+                # if @unlikely(tl <= UInt64(31))
+                0x1.ecf3fep-73, 0x1.f8a754p-9,
+                0x1.8d16b2p+5, 0x1.87bdfp+115,
+                -0x1.25cb66p-123, -0x1.c2f04p-30,
+                -0x1.580c1ep+1, -0x1.efc2a2p+14,
+            ])
+        end
+        test_x = [test_x..., -test_x...]
+        @testset "cr_lgamma($(repr(x)))" for x in test_x
+            # Test against system libm
+            # SpecialFunctions.lgamma: acc workaround
+            #   x in [4.0850034f36, -3.1435504f0, -2.4569082f0]
+            # TODO: @test PureLibm.cr_lgamma(x) ≈ SpecialFunctions.lgamma(x)
+            # Test against MPFR
+            @test PureLibm.cr_lgamma(x) === T(SpecialFunctions.lgamma(BigFloat(x)))
+        end
     end
 end
+
+pos_range = (lo=Float32(0.0), hi=nextfloat(Float32(0x1.895f1cp+121)))
+neg_range = (lo=Float32(-0.0), hi=nextfloat(-Float32(Inf)))
+if "cr_lgamma.fast" in CheckExhaustive
+    @testset "cr_lgamma-exhaustive.fast" begin
+        test_float_range(SpecialFunctions.lgamma, PureLibm.cr_lgamma, lo=pos_range.lo, hi=pos_range.hi)
+        test_float_range(SpecialFunctions.lgamma, PureLibm.cr_lgamma, lo=neg_range.lo, hi=neg_range.hi)
+    end
+end
+if "cr_lgamma" in CheckExhaustive
+    @testset "cr_lgamma-exhaustive" begin
+        test_float_range(SpecialFunctions.lgamma, PureLibm.cr_lgamma, lo=pos_range.lo, hi=pos_range.hi, bigfloat=true)
+        test_float_range(SpecialFunctions.lgamma, PureLibm.cr_lgamma, lo=neg_range.lo, hi=neg_range.hi, bigfloat=true)
+    end
+end
+# ENV["PURELIBM_CHECK_EXHAUSTIVE"] = "cr_lgamma.fast,cr_lgamma"
