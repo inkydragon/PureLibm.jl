6952cdfe2a855b495c85f4884284b15059fb9ff6
Chengyu Han <cyhan.dev@outlook.com>
2025-11-27 01:16:05 +0800

    erff: port from core-math (#70)
    * erff: port from core-math
    
    * erff.doc: add examples
    
    * test.erf: add tests
    
    * erff.doc: fix typo
    
    * test.erf: import SpecialFunctions
    
    * test.erf: remove sources in test\Project.toml

--- test/erf.jl
+++ test/erf.jl
@@ -1,29 +1,62 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0
+import SpecialFunctions
+
 
 for T in (Float32, )
     @testset "cr_erf($T)" begin
-        @test_throws ErrorException PureLibm.cr_erf(T(1.0))
         # IEC 60559
-        # @test isnan(PureLibm.cr_erf(T(NaN)))
-        # # erf(±0) returns ±0
-        # @test PureLibm.cr_erf(T(0)) == T(0)
-        # @test PureLibm.cr_erf(-T(0)) == -T(0)
-        # # erf(±∞) returns ±1
-        # @test PureLibm.cr_erf(T(Inf)) == T(1)
-        # @test PureLibm.cr_erf(-T(Inf)) == -T(1)
-
+        @test isnan(PureLibm.cr_erf(T(NaN)))
+        # erf(±0) returns ±0
+        @test PureLibm.cr_erf(T(0)) == T(0)
+        @test PureLibm.cr_erf(-T(0)) == -T(0)
+        # erf(±∞) returns ±1
+        @test PureLibm.cr_erf(T(Inf)) == T(1)
+        @test PureLibm.cr_erf(-T(Inf)) == -T(1)
     end
 
-    # @testset "cr_erf(rand($T))" begin
-    #     test_x = T[
-    #         eps(T(0.0)),
-    #     ]
-    #     @testset "cr_erf($x)" for x in test_x
-    #         res = PureLibm.cr_erf(x)
-    #         # Test against system libm
-    #         @test res ≈ erf(x)
-    #         # Test against MPFR
-    #         @test res === T(erf(BigFloat(x)))
-    #     end
-    # end
+    @testset "cr_erf(rand($T))" begin
+        test_x = T[
+            eps(T(0.0)),
+            rand_float(T(0.0), T(0.5), 16)...,
+            rand_float(T(0.5), T(4.0), 16)...,
+        ]
+        if Float32 == T
+            # Branch cov
+            append!(test_x, T[
+                # if @unlikely(uax > 0x407ad444)
+                #   |x| > 3.9192057f0 (0x1.f5a888p+1)
+                3.9192057f0, nextfloat(3.9192057f0),
+                rand_float(T(3.9), T(Inf), 32)...,
+                # if @unlikely(uax < 0x3ee0_0000)
+                #   |x| < 0.4375f0 (0x1.cp-2)
+                prevfloat(0.4375f0),
+                0.4375f0,
+                nextfloat(0.4375f0),
+                rand_float(T(0.0), 0.4375f0, 32)...,
+            ])
+        end
+        @testset "cr_erf($(repr(x)))" for x in test_x
+            res = PureLibm.cr_erf(x)
+            # Test against system libm
+            @test res ≈ SpecialFunctions.erf(x)
+            # Test against MPFR
+            @test res === T(SpecialFunctions.erf(BigFloat(x)))
+        end
+    end
 end
+
+pos_range = (lo=Float32(0.0), hi=Float32(4.0))
+neg_range = (lo=Float32(-0.0), hi=Float32(-4.0))
+if "cr_erf.fast" in CheckExhaustive
+    @testset "cr_erf-exhaustive.fast" begin
+        test_float_range(SpecialFunctions.erf, PureLibm.cr_erf, lo=pos_range.lo, hi=pos_range.hi)
+        test_float_range(SpecialFunctions.erf, PureLibm.cr_erf, lo=neg_range.lo, hi=neg_range.hi)
+    end
+end
+if "cr_erf" in CheckExhaustive
+    @testset "cr_erf-exhaustive" begin
+        test_float_range(SpecialFunctions.erf, PureLibm.cr_erf, lo=pos_range.lo, hi=pos_range.hi, bigfloat=true)
+        test_float_range(SpecialFunctions.erf, PureLibm.cr_erf, lo=neg_range.lo, hi=neg_range.hi, bigfloat=true)
+    end
+end
+# ENV["PURELIBM_CHECK_EXHAUSTIVE"] = "cr_erf.fast,cr_erf"
