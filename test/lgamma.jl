d87e2620a7c3847df8197db2757cdd926a036242
Chengyu HAN <cyhan.dev@outlook.com>
2025-11-26 14:51:32 +0800

    test.lgamma: add tests

--- test/lgamma.jl
+++ test/lgamma.jl
@@ -3,22 +3,21 @@ import SpecialFunctions
 
 for T in (Float32, )
     @testset "cr_lgamma($T)" begin
-        @test_throws ErrorException PureLibm.cr_lgamma(T(1.0))
         # IEC 60559
-        # @test isnan(PureLibm.cr_lgamma(T(NaN)))
-        # # — lgamma(1) returns +0
-        # @test PureLibm.cr_lgamma(T(1)) == T(0.0)
-        # # — lgamma(2) returns +0
-        # @test PureLibm.cr_lgamma(T(2)) == T(0.0)
-        # # — lgamma(x) returns +∞ and raises the "divide-by-zero" floating-point exception
-        # #   for x a negative integer or zero.
-        # @test PureLibm.cr_lgamma(T(0.0)) == T(Inf)
-        # @test PureLibm.cr_lgamma(-T(1)) == T(Inf)
-        # @test PureLibm.cr_lgamma(-T(2)) == T(Inf)
-        # @test PureLibm.cr_lgamma(-T(100)) == T(Inf)
-        # # — lgamma(−∞) returns +∞
-        # @test PureLibm.cr_lgamma(T(-Inf)) == T(Inf)
-        # # — lgamma(+∞) returns +∞
-        # @test PureLibm.cr_lgamma(T(Inf)) == T(Inf)
+        @test isnan(PureLibm.cr_lgamma(T(NaN)))
+        # — lgamma(1) returns +0
+        @test PureLibm.cr_lgamma(T(1)) == T(0.0)
+        # — lgamma(2) returns +0
+        @test PureLibm.cr_lgamma(T(2)) == T(0.0)
+        # — lgamma(x) returns +∞ and raises the "divide-by-zero" floating-point exception
+        #   for x a negative integer or zero.
+        @test PureLibm.cr_lgamma(T(0.0)) == T(Inf)
+        @test PureLibm.cr_lgamma(-T(1)) == T(Inf)
+        @test PureLibm.cr_lgamma(-T(2)) == T(Inf)
+        @test PureLibm.cr_lgamma(-T(100)) == T(Inf)
+        # — lgamma(−∞) returns +∞
+        @test PureLibm.cr_lgamma(T(-Inf)) == T(Inf)
+        # — lgamma(+∞) returns +∞
+        @test PureLibm.cr_lgamma(T(Inf)) == T(Inf)
     end
 end
