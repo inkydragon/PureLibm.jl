a2e32010318844609105d0441a1b1e988e3cd8f3
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-04 10:37:11 +0800

    test.sqrt

--- test/sqrt.jl
+++ test/sqrt.jl
@@ -2,6 +2,8 @@
 
 for T in (Float32, )
     @testset "cr_sqrt($T)" begin
+        float_gen = Data.Floats{T}(; nans=false, infs=true)
+
         # IEC 60559
         @test isnan(PureLibm.cr_sqrt(T(NaN)))
         # sqrt(±0) returns ±0
@@ -13,5 +15,9 @@ for T in (Float32, )
         #   for x < 0
         # @test isnan(PureLibm.cr_sqrt(T(-1.0)))
         # @test isnan(PureLibm.cr_sqrt(T(-Inf)))
+        # @testset "sqrt(x) = NaN, for x < 0" begin
+        #     f_domain = filter(x -> x < 0, float_gen)
+        #     @check sqrt_domain(f = f_domain) = isnan(PureLibm.cr_sqrt(f))
+        # end
     end
 end
