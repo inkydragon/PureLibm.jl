0323cd3cc5006f84ed903e05906cf4daee8fb64c
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-04 10:43:25 +0800

    test.lgamma

--- test/lgamma.jl
+++ test/lgamma.jl
@@ -16,6 +16,11 @@ for T in (Float32, )
         @test PureLibm.cr_lgamma(-T(1)) == T(Inf)
         @test PureLibm.cr_lgamma(-T(2)) == T(Inf)
         @test PureLibm.cr_lgamma(-T(100)) == T(Inf)
+        @testset "lgamma(x) = -Inf, for negative integer or zero x" begin
+            int_gen = Data.Integers{Int64}()
+            f_domain = filter(x -> x <= 0, int_gen)
+            @check lgamma_domain(f = f_domain) = PureLibm.cr_lgamma(T(f)) == T(Inf)
+        end
         # — lgamma(−∞) returns +∞
         @test PureLibm.cr_lgamma(T(-Inf)) == T(Inf)
         # — lgamma(+∞) returns +∞
