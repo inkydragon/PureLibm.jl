2f7f76fa274358ddab921ad1b319bf163bfb394b
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-08 00:03:19 +0800

    const: add Float64 func

--- src/utils/const.jl
+++ src/utils/const.jl
@@ -5,35 +5,51 @@
 - IEEE 754
 - [IEEE Support Functions - Oracle® Developer Studio 12.5: Numerical Computation Guide](https://docs.oracle.com/cd/E60778_01/html/E60763/z4000ac120180.html#OSSNCz4000ac119391)
 - [Single-precision floating-point format - Wikipedia](https://en.wikipedia.org/wiki/Single-precision_floating-point_format#Notable_single-precision_cases)
+- [Double-precision floating-point format - Wikipedia](https://en.wikipedia.org/wiki/Double-precision_floating-point_format)
 """
 
 """Quiet NaN32"""
 const QNaN32 = reinterpret(Float32, 0x7fffffff)
 """Signaling NaN32"""
 const SNaN32 = reinterpret(Float32, 0x7f800001)
+"""Quiet NaN64"""
+const QNaN64 = reinterpret(Float64, 0x7fffffff_ffffffff)
+"""Signaling NaN64"""
+const SNaN64 = reinterpret(Float64, 0x7ff00000_00000001)
 
 
 """Quiet NaN"""
+quiet_nan
 quiet_nan(::Type{Float32}) = QNaN32
+quiet_nan(::Type{Float64}) = QNaN64
+
 """Signaling NaN"""
+signaling_nan
 signaling_nan(::Type{Float32}) = SNaN32
+signaling_nan(::Type{Float64}) = SNaN64
 
 """
 Max Normal Float
 """
+normal_max
 normal_max(::Type{T}) where {T<:AbstractFloat} = floatmax(T)
 
 """
 Min Normal Float
 """
+normal_min
 normal_min(::Type{T}) where {T<:AbstractFloat} = floatmin(T)
 
 """
 Max Subnormal Float
 """
+subnormal_max
 subnormal_max(::Type{Float32}) = reinterpret(Float32, 0x007fffff)
+subnormal_max(::Type{Float64}) = reinterpret(Float64, 0x000fffff_ffffffff)
 
 """
 Min Subnormal Float
 """
+subnormal_min
 subnormal_min(::Type{Float32}) = reinterpret(Float32, 0x00000001)
+subnormal_min(::Type{Float64}) = reinterpret(Float64, 0x00000000_00000001)
--- test/utils/const.jl
+++ test/utils/const.jl
@@ -9,7 +9,9 @@ const F32_NEG_RANGE = (lo=-Float32(0.0), hi=-Float32(Inf))
 """Float32 (-Inf, -0.0]"""
 const F32_NEG_FINITE_RANGE = (lo=-Float32(0.0), hi=-prevfloat(Float32(Inf)))
 
+
 asfloat(u::UInt32) = reinterpret(Float32, u)
+asfloat(u::UInt64) = reinterpret(Float64, u)
 
 """
 NO   Name           Hex         hex: %a             base2               base10
@@ -56,4 +58,25 @@ NO   Name           Hex          hex: %a             base2               base10
     @test asfloat(0xff7fffff) === -PureLibm.normal_max(Float32)
     @test isnan(-PureLibm.SNaN32)
     @test isnan(-PureLibm.QNaN32)
+end
+
+@testset "Const :: Float64" begin
+    # NOTE: DoNot use `===` to compare NaN
+    @test isnan(PureLibm.QNaN64)
+    @test isnan(PureLibm.SNaN64)
+    @test isnan(PureLibm.quiet_nan(Float64))
+    @test isnan(PureLibm.signaling_nan(Float64))
+    @test PureLibm.normal_max(Float64) === Float64(0x1.fffffffffffffp+1023)
+    @test PureLibm.normal_min(Float64) === Float64(0x1p-1022)
+    @test PureLibm.subnormal_max(Float64) === Float64(0x1.ffffffffffffep-1023)
+    @test PureLibm.subnormal_min(Float64) === Float64(0x1p-1074)
+    @test zero(Float64) === Float64(0x0p+0)
+    # <0
+    @test asfloat(0x80000000_00000000) === -zero(Float64) 
+    @test asfloat(0x80000000_00000001) === -PureLibm.subnormal_min(Float64)
+    @test asfloat(0x800fffff_ffffffff) === -PureLibm.subnormal_max(Float64)
+    @test asfloat(0x80100000_00000000) === -PureLibm.normal_min(Float64)
+    @test asfloat(0xffefffff_ffffffff) === -PureLibm.normal_max(Float64)
+    @test isnan(-PureLibm.SNaN64)
+    @test isnan(-PureLibm.QNaN64)
 end
