75f3ae0059bd880a5bb146789a43e2f9da1408f6
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-07 14:56:59 +0800

    cosf: format code

--- src/core-math/binary32/cosf.jl
+++ src/core-math/binary32/cosf.jl
@@ -2,6 +2,7 @@
 # Based on core-math/src/binary32/cos/cosf.c
 # CORE-MATH project Copyright (c) 2022-2023 Alexei Sibidanov.
 
+#! format: off
 const CR_COSF_TB = NTuple{32, Float64}((
     0x1p+0, 0x1.f6297cff75cbp-1, 0x1.d906bcf328d46p-1, 0x1.a9b66290ea1a3p-1,
     0x1.6a09e667f3bcdp-1, 0x1.1c73b39ae68c8p-1, 0x1.87de2a6aea963p-2, 0x1.8f8b83c69a60bp-3,
@@ -12,45 +13,6 @@ const CR_COSF_TB = NTuple{32, Float64}((
     0x0p+0, 0x1.8f8b83c69a60bp-3, 0x1.87de2a6aea963p-2, 0x1.1c73b39ae68c8p-1,
     0x1.6a09e667f3bcdp-1, 0x1.a9b66290ea1a3p-1, 0x1.d906bcf328d46p-1, 0x1.f6297cff75cbp-1
 ))
-
-
-function _cosf_absc(z::Float64, ia::Int64)
-    z2 = z * z
-    z4 = z2 * z2
-    a, b = CR_SINF_A, CR_SINF_B
-    aa = (a[1] + z2 * a[2]) + z4 * (a[3] + z2 * a[4])
-    bb = (b[1] + z2 * b[2]) + z4 * (b[3] + z2 * b[4])
-    s0 = CR_COSF_TB[((ia+8) & 31) + 1]
-    c0 = CR_COSF_TB[(ia & 31) + 1]
-    return aa, bb, s0, c0
-end
-
-function _cosf_big(x::Float32)::Float32
-    tu = reinterpret(UInt32, x)
-    ax = tu << 1
-    if @unlikely(ax >= (UInt32(0xff) << 24))
-        # nan or +-inf
-        if (ax << 8) != 0
-            # NaN
-            return x + x  
-        end
-        # to raise FE_INVALID
-        return 0.0f0 / 0.0f0 
-    end
-
-    z, ia = _sinf_rbig(tu)
-    aa, bb, s0, c0 = _cosf_absc(z, ia)
-    r = c0 + z*(aa*s0 - bb*(z*c0))
-    tru = reinterpret(UInt64, r)
-
-    tail = (tru + UInt64(6)) & (~UInt64(0) >> 36);
-    if tail <= 12
-        return _cosf_database(x, r)
-    end
-
-    return r
-end
-
 
 const CR_COSF_ST = Vector{Tuple{UInt32, Float32, Float32}}([
     # 4.712389f0 (0x1.2d97c8p+2)
@@ -64,6 +26,44 @@ const CR_COSF_ST = Vector{Tuple{UInt32, Float32, Float32}}([
     # 1.7269983f20 (0x1.2b9622p+67)
     (0x6115cb11, Float32(0x1.f0285ep-1), Float32(-0x1p-26))
 ])
+#! format: on
+
+function _cosf_absc(z::Float64, ia::Int64)
+    z2 = z * z
+    z4 = z2 * z2
+    a, b = CR_SINF_A, CR_SINF_B
+    aa = (a[1] + z2 * a[2]) + z4 * (a[3] + z2 * a[4])
+    bb = (b[1] + z2 * b[2]) + z4 * (b[3] + z2 * b[4])
+    s0 = CR_COSF_TB[((ia+8)&31)+1]
+    c0 = CR_COSF_TB[(ia&31)+1]
+    return aa, bb, s0, c0
+end
+
+function _cosf_big(x::Float32)::Float32
+    tu = reinterpret(UInt32, x)
+    ax = tu << 1
+    if @unlikely(ax >= (UInt32(0xff) << 24))
+        # nan or +-inf
+        if (ax << 8) != 0
+            # NaN
+            return x + x
+        end
+        # to raise FE_INVALID
+        return 0.0f0 / 0.0f0
+    end
+
+    z, ia = _sinf_rbig(tu)
+    aa, bb, s0, c0 = _cosf_absc(z, ia)
+    r = c0 + z * (aa * s0 - bb * (z * c0))
+    tru = reinterpret(UInt64, r)
+
+    tail = (tru + UInt64(6)) & (~UInt64(0) >> 36)
+    if tail <= 12
+        return _cosf_database(x, r)
+    end
+
+    return r
+end
 
 function _cosf_database(x::Float32, r::Float64)::Float32
     tu = reinterpret(UInt32, x)
@@ -140,6 +140,6 @@ function cr_cosf(x::Float32)::Float32
 
     aa, bb, s0, c0 = _cosf_absc(z, ia)
     z2 = z * z
-    r = c0 + aa*(z*s0) - bb*(z2*c0)
+    r = c0 + aa * (z * s0) - bb * (z2 * c0)
     return r
 end
