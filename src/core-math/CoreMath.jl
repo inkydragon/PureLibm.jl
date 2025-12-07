a1ba44eaa31ba1162a780c63343f8c8f847c17c7
Chengyu Han <cyhan.dev@outlook.com>
2025-12-07 15:02:43 +0800

    cosf: add ref, examples, format code, export (#83)
    * cosf: add ref
    
    * cosf: add examples
    
    * cosf: format code
    
    * cm: export cr_cos

--- src/core-math/CoreMath.jl
+++ src/core-math/CoreMath.jl
@@ -4,7 +4,7 @@ include("helper.jl")
 
 #= Trigonometric =#
 export cr_acos, cr_asin, cr_atan
-export cr_sin
+export cr_cos, cr_sin
 include("binary32/acosf.jl")
 include("binary32/asinf.jl")
 include("binary32/atanf.jl")
