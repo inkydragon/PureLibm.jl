9a2f72d9b4ecacd75763be9a4bcff28c45111d9f
Chengyu HAN <cyhan.dev@outlook.com>
2025-12-08 17:37:31 +0800

    doc.asin[pi]: update Examples

--- src/core-math/binary32/asinf.jl
+++ src/core-math/binary32/asinf.jl
@@ -45,25 +45,22 @@ Correctly-rounded arc-sine function for `Float32`.
 
 # Examples
 ```jldoctest
-julia> cr_asin(-1.0f0) / pi
--0.5f0
+julia> cr_asin.(Float32[-1.0, -0.5, -0.0, 0.0, 0.5, 1.0]) / pi
+6-element Vector{Float32}:
+ -0.5
+ -0.16666667
+ -0.0
+  0.0
+  0.16666667
+  0.5
 
-julia> cr_asin(-0.5f0) / pi  # -1/6
--0.16666667f0
+julia> cr_asin(0.5f0) === Float32(pi/6)
+true
 
-julia> cr_asin(-0.0f0) / pi
--0.0f0
+julia> cr_asin(nextfloat(1.0f0))   # |x| > 1, domain error
+NaN32
 
-julia> cr_asin(0.0f0) / pi
-0.0f0
-
-julia> cr_asin(0.5f0) / pi
-0.16666667f0
-
-julia> cr_asin(1.0f0) / pi
-0.5f0
-
-julia> cr_asin(NaN32)
+julia> cr_asin(-nextfloat(1.0f0))
 NaN32
 
 julia> cr_asin(Inf32)
--- src/core-math/binary32/asinpif.jl
+++ src/core-math/binary32/asinpif.jl
@@ -48,13 +48,22 @@ This function computes `asin(x)/π`
 
 # Examples
 ```jldoctest
-julia> PureLibm.cr_asinpi(0.0f0)
-0.0f0
+julia> PureLibm.cr_asinpi.(Float32[-1.0, -0.5, -0.0, 0.0, 0.5, 1.0])
+6-element Vector{Float32}:
+ -0.5
+ -0.16666667
+ -0.0
+  0.0
+  0.16666667
+  0.5
 
-julia> PureLibm.cr_asinpi.((1.0f0, -1.0f0))
-(0.5f0, -0.5f0)
+julia> PureLibm.cr_asinpi(0.5f0) === Float32(1/6)
+true
 
-julia> PureLibm.cr_asinpi(nextfloat(1.0f0))
+julia> PureLibm.cr_asinpi(nextfloat(1.0f0))     # |x| > 1, domain error
+NaN32
+
+julia> PureLibm.cr_asinpi(-nextfloat(1.0f0))
 NaN32
 
 julia> PureLibm.cr_asinpi(Inf32)
